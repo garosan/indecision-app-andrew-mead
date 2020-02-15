c0aa8db5e8bae9733876c6a1402066cd68ac7ea8
Edgar Sanchez <garosanchez27@gmail.com>
2020-02-15 13:44:28 -0600

    Add ReactDOM.render with its 2 arguments

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,3 +2,6 @@ console.log("App.js is running");
 
 // JSX - Javascript XML
 var template = <p>This is JSX inside app.js</p>;
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
